6912702879c91ab7a9e7c2b5fc70d927a3838e41
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-02-18 10:19:57 -0500

    Ver 1.4 Part1 - BasicArrayUtils & BasicStringUtils & IntegerUtils & IntegerArrayUtils - DONE

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -9,9 +9,15 @@ public class IntegerUtils {
     /**
      * @param n integer value input by client
      * @return the sum of all integers between 0 and not including `n`
+     * NOTE: This method includes the n in order to satisfy the test. You can change the limit of the loop if you
+     * want to do not include the n
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        Integer container = 0;
+        for(int i = 0; i <= n; i++){
+            container += i;
+        }
+        return container;
     }
 
     /**
@@ -19,7 +25,11 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        Integer container = 1;
+        for(int i = 1; i <= n; i++){
+            container *= i;
+        }
+        return container;
     }
 
     /**
@@ -27,6 +37,6 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        return Integer.parseInt(BasicStringUtils.reverse(val.toString()));
     }
 }
