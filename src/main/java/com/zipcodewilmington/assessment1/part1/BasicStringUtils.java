df9ccee297d297924815410b6ad5f28c6538b1ab
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-02-18 09:55:08 -0500

    Ver 1.2 Part1 - BasicArrayUtils & BasicStringUtils - DONE

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,7 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        return str.substring(0,1).toUpperCase()+str.substring(1,str.length());
     }
 
     /**
@@ -17,7 +17,11 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String container = "";
+        for( int i = str.length(); i > 0; i--){
+            container += str.substring(i - 1, i);
+        }
+        return container;
     }
 
     /**
@@ -25,7 +29,7 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        return camelCase(reverse(str));
     }
 
 
@@ -34,14 +38,32 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1,str.length()-1);
     }
 
     /**
      * @param str a string input from user
      * @return string with identical characters, each with opposite casing
      */
+//    String input = "tHE quiCK brOwN";
+//    String expected = "The QUIck BRoWn";
     public static String invertCasing(String str) {
-        return null;
+        String container = "";
+        for (int i = 0  ; i < str.length(); i++) {
+            container += invertLetter(str.substring(i,i+1).charAt(0));
+        }
+        return container;
+    }
+
+    public static String invertLetter(Character c){
+        if( c != ' '){
+            if( Character.isUpperCase( c ) ){
+                return c.toString().toLowerCase();
+            }
+            else {
+                return c.toString().toUpperCase();
+            }
+        }
+        return " ";
     }
 }
